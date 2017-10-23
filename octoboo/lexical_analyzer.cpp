26cf8c25a0f6f6066370308675b5fb1bb1c769af
Cindy Dallaire <c.dallaire93@gmail.com>
2017-10-22 21:38:28 -0400

    Add beginning of lexical analyzing for symbols

--- octoboo/lexical_analyzer.cpp
+++ octoboo/lexical_analyzer.cpp
@@ -1,10 +1,39 @@
 #include "lexical_analyzer.h"
+#include "token_code.h"
 
 Token* LexicalAnalyzer::GetNextToken()
 {
-	if (m_Stream->is_open() && !m_Stream->eof())
+	if (!m_Stream->is_open() || m_Stream->eof()) return nullptr;
+
+	char c = m_Stream->get();
+
+	if (isalpha(c))//q0 -> q1
 	{
-		
+		std::string identifier = std::string();
+		do
+		{
+			identifier.append(1, c);
+			c = m_Stream->get();
+		} while (!isspace(c) && c != ';');
+
+		return new Token(identifier, TokenCode::identifier);
 	}
-	return nullptr;
+	else if(isdigit(c))//q0 -> q2
+	{
+		do
+		{
+			c = m_Stream->get();
+		} while (!isspace(c));
+	}
+	else//q0->qsymbol
+	{
+		if (c == '+') return new Token(std::string(c, 1), TokenCode::add_op);
+		if (c == '-') return new Token(std::string(c, 1), TokenCode::sub_op);
+		if (c == '*') return new Token(std::string(c, 1), TokenCode::mul_op);
+		if (c == '\\') return new Token(std::string(c, 1), TokenCode::div_op);
+		if (c == '=') return new Token(std::string(c, 1), TokenCode::assign_op);
+		if (c == ';') return new Token(std::string(c, 1), TokenCode::semi_colon);
+		if (c == '(') return new Token(std::string(c, 1), TokenCode::left_parenthesis);
+		if (c == ')') return new Token(std::string(c, 1), TokenCode::right_parenthesis);
+	}
 }
